29c689c98ad8b9f4487452ab5072635e4d09b8bd
Aromal <82868370+AROMAL642@users.noreply.github.com>
2024-01-01 21:57:20 +0530

    Update Voice_Assistant.py

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -10,46 +10,82 @@ import pyttsx3
 import vosk
 import pyaudio
 import requests
+import pickle
+import cv2
+import subprocess
+import re
+
+
+
+
+
+#def load_and_cache_directory(directory_path, cache_file):
+ #   if os.path.exists(cache_file):
+       
+  #      with open(cache_file, 'rb') as f:
+   #         cached_data = pickle.load(f)
+    #    print(f"Loaded directory from cache: {directory_path}")
+      #  return cached_data
+
+    #if os.path.exists(directory_path) and os.path.isdir(directory_path):
+      
+     #   loaded_directory = os.listdir(directory_path)
+
+        
+    #    with open(cache_file, 'wb') as f:
+     #       pickle.dump(loaded_directory, f)
+      #  print(f"Cached directory: {directory_path}")
+       # return loaded_directory
+
+   # else:
+    #    print("The specified path does not exist or is not a directory.")
+     #   return []
+
+#if __name__ == "__main__":
+ #   directory_path = "/home/aromal/Desktop/voice_assistant_project/vosk-model-en-in-0.5"
+  #  cache_file = "directory_cache.pkl"
+
+   
+#directory=load_and_cache_directory(directory_path,cache_file)
+
+
+
+
+
+
+
 
 num = 1
+name="Aliya"
+def assistant_name():
+	name="I am liya"
+	speak_online_or_offline(name)
 
+#"/home/aromal/Desktop/voice_assistant_project/vosk-model-en-in-0.5"
 
-
-
-def speak_on_or_off(output):
+def is_internet_available():
     try:
-      
-        response = requests.get('http://www.google.com', timeout=5)
-        if response.status_code==200:
-            
-            assistant_speaks(output)
-        else:
-           
-           speak(output)  
         
+        response = requests.get("https://www.google.com", timeout=2)
+       
+        return response.status_code == 200
     except requests.ConnectionError:
+        
         return False
-        
 
-                    
-        
-def recognize_on_or_off():
-    try:
-        # Send a request to a reliable online service (e.g., Google DNS)
-        response = requests.get('http://www.google.com', timeout=5)
-    
-        r="true"
-        if r=="true":
-         get_audio()
-        else:
-        
-         offline_sr()
-    except requests.ConnectionError:
-        print("error")
-            
+def assistant_speaks(output):                      #speak on online
+	global num
 
+	num += 1
+	print(name,":",output)
 
+	toSpeak = gTTS(text = output, lang ='en', slow = False)
 
+	file = str(num)+".mp3"
+	toSpeak.save(file)
+	playsound.playsound(file, True)
+	os.remove(file)
+ 
 def speak(output):# offline speaking
    
     engine = pyttsx3.init()
@@ -60,39 +96,16 @@ def speak(output):# offline speaking
     engine.say(output)
     engine.runAndWait()
     
-    
-def assistant_speaks(output):                      #speak on online
-	global num
-
-	num += 1
-	print("PerSon : ", output)
-
-	toSpeak = gTTS(text = output, lang ='en', slow = False)
-
-	file = str(num)+".mp3"
-	toSpeak.save(file)
-	playsound.playsound(file, True)
-	os.remove(file)
-    
-
-
-
-def offline_sr(model_path="/home/aromal/vosk-model-small-en-us-0.15",sample_rate=16000):    #offline voice Recognizer
+def offline_sr(model_path="/home/aromal/Desktop/voice_assistant_project/vosk-model-en-in-0.5",sample_rate=16000,):    #offline voice Recognizer
 
     if not os.path.exists(model_path):
         print(f"Vosk model not found at {model_path}")
         return
 
     vosk_model = vosk.Model(model_path)
-
-    
     p = pyaudio.PyAudio()
-
-    
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=sample_rate, input=True, frames_per_buffer=1024)
-
     recognizer = vosk.KaldiRecognizer(vosk_model, sample_rate)
-
     print("Listening...")
 
     try:
@@ -110,8 +123,6 @@ def offline_sr(model_path="/home/aromal/vosk-model-small-en-us-0.15",sample_rate
         pass
 
     print("Recognition finished.")
-
-
     stream.stop_stream()
     stream.close()
     p.terminate()
@@ -127,9 +138,8 @@ def get_audio():                          #online voice Recognizer
 	with sr.Microphone() as source:
 		print("Speak...")
 		
-		# recording the audio using speech recognition
-		audio = rObject.listen(source, phrase_time_limit = 5)
-	print("Stop.") # limit 5 secs
+		audio = rObject.listen(source, phrase_time_limit = 4)
+	print("Stop.") # 
 
 	try:
 
@@ -139,15 +149,58 @@ def get_audio():                          #online voice Recognizer
 
 	except:
 
-		speak_on_or_off("Could not understand your audio, PLease try again !")
+		speak_online_or_offline("Could not understand your audio, PLease try again !")
 		return 0
+    
+ 
+
+def speak_online_or_offline(output):
+    if is_internet_available():
+        assistant_speaks(output)
+    else:
+        speak(output)
+        
+def recognize_online_or_offline():
+    if is_internet_available():
+        
+        return get_audio()
+    else:
+        return offline_sr()        
 
 
-# Driver Code
-name="Aliya"
-def assistant_name():
-	name="I am liya"
-	speak_on_or_off(name)
+		
+def calculate_numbers(text):
+	word_list_calculation={"1","2","3","4","5","6","6","7","8","9","0"}
+	words_in_sentence = text.split()
+	print(words_in_sentence)
+	numbers = []
+	
+	for word in words_in_sentence:
+		if word in word_list_calculation:
+			numbers.append(word)
+			print(numbers)
+		#query = text.replace(str(word_list_addition), "").strip()
+		break
+
+
+
+
+
+def opensettings(text):
+	if "settings" in text.lower():
+
+		assistant_speaks("settings app oppened")
+		subprocess.run(['gnome-control-center'])
+	
+	return recognize_online_or_offline()
+
+
+
+
+
+
+
+
 
 
 
@@ -159,57 +212,60 @@ def search():
 
 	if 'youtube' in str(text):
 
-		speak_on_or_off("Opening in youtube")
+		speak_online_or_offline("Opening in youtube")
 	    
 		query = text.replace("search", "").strip()
-		speak_on_or_off(query)
+		speak_online_or_offline(query)
 		driver.get(f"https://www.youtube.com/results?search_query={query}")
 		return
 
 	elif 'google' in str(text):
-		speak_on_or_off("Searching on google...pleace wait")
+		speak_online_or_offline("Searching on google...pleace wait")
         
 		word_list={"search","search in","in google","find in","find","on google"}
 		query = text.replace(word_list, "").strip()
-		speak_on_or_off(query)
+		speak_online_or_offline(query)
 		driver.get("https://www.google.com/search?client=firefox-b-e&q={query}")
 		return	
 	
 	elif 'firefox' in str(text) or 'search on web' in str(text) or 'search' in str(text):
-		speak_on_or_off("Searching on Web...pleace wait")
+		speak_online_or_offline("Searching on Web...pleace wait")
         
 
 		word_list={"search","search in","in firefox","find in","find","on firefox"}
 		query = text.replace(str(word_list), "").strip()
-		speak_on_or_off(query)
+		speak_online_or_offline(query)
 		driver.get(f"https://www.google.com/search?client=firefox-b-e&q={query}")
 		return
    
 
 if __name__ == "__main__":
 
-	speak_on_or_off("Can I Help You Sir?")
-	answer=recognize_on_or_off()
+	speak_online_or_offline("Can I Help You Sir?")
+	answer=recognize_online_or_offline()
+           
+    
 	if "yes" in str(answer) or "of course" in str(answer) or "sure" in str(answer) or "yeah" in str(answer) or "ok" in str(answer):
 		access="grant"
 	
 	
-	while(access=="grant"):
+ 
+while(access=="grant"):
 
-		speak_on_or_off("What can i do for you?")
-		text = recognize_on_or_off().lower()
+		speak_online_or_offline("What can i do for you?")
+		text = recognize_online_or_offline().lower()
 
 		if text == 0:
 			continue
 		
 		elif "who are you" in str(text) or "deffine yourself" in str(text):
-			speak_on_or_off('''Hello, I am a virtual Person. Your personal Assistant.
+			speak_online_or_offline('''Hello, I am a virtual Person. Your personal Assistant.
 		    Developed by Aromal''')
 			continue
 		
 		
 		elif "who made you" in str(text) or "who developed you" in str(text) or "who invent" in str(text):
-			speak_on_or_off('''The one and only Genious Aromal''')
+			speak_online_or_offline('''The one and only Genious Aromal''')
 			continue
 			
 		elif "what is your name" in str(text) or "your name" in str(text) or "name of you" in str(text):
@@ -221,12 +277,24 @@ if __name__ == "__main__":
 			continue
 
 		elif "search" in str(text) or "find" in str(text)or "play" in str(text):
-			speak_on_or_off("Iam Trying to find your Result...Pleace wait..")
+			speak_online_or_offline("Iam Trying to find your Result...Pleace wait..")
 			search()
-			continue	
+			continue
+    
 
 		elif "exit" in str(text) or "bye" in str(text) or "sleep" in str(text)or "shut up" in str(text)or "get lost" in str(text)or "leave me" in str(text) or "nothing" in str(text):
-			speak("Ok bye, ")
+			speak_online_or_offline("Ok bye, ")
+			break
+      
+		elif "calculate" in str(text) or "multiply" in str(text) or "devide" in str(text)or "add" in str(text)or "substract" in str(text):
+			calculate_numbers(text)
+			break
+		elif "open" in str(text) or "run" in str(text) or "goto" in str(text):
+			opensettings(text)
+			break
+
+		elif "set" in str(text) or "make" in str(text) or "adjust" in str(text):
+			extract_numbers(text)
 			break
 
 
@@ -237,13 +305,8 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
             
-		# calling proceses text to process the query
+	
 		
 def process_text(text):
 	try:
